f7035676541cb310d0c78cec2e93e49f3b91c43d
sidadrian3 <sidadrian3@gmail.com>
2025-04-24 23:09:16 +0800

    no more theme

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -76,12 +76,12 @@ export default function RootLayout({
         />
       </head>
       <body className="antialiased flex flex-col items-center justify-center mx-auto mt-2 lg:mt-8 mb-20 lg:mb-40">
-        <ThemeProvider
+        {/* <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
-        >
+        > */}
           <main className="flex-auto min-w-0 mt-2 md:mt-6 flex flex-col px-6 sm:px-4 md:px-0 max-w-[624px] w-full">
             <Navbar />
             {children}
@@ -89,7 +89,7 @@ export default function RootLayout({
             <Analytics />
             <SpeedInsights />
           </main>
-        </ThemeProvider>
+        {/* </ThemeProvider> */}
       </body>
     </html>
   );

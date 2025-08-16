00703885774d1cc9d17b443aa4b8e87aa0eb748f
sidadrian3 <sidadrian3@gmail.com>
2025-08-16 13:31:35 +0800

    feat: add GridContacts component and update Contact page layout

--- my-portfolio/app/components/nav.tsx
+++ my-portfolio/app/components/nav.tsx
@@ -24,7 +24,7 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <nav className=" lg:mb-16 mb-12 py-5">
+    <nav className=" lg:mb-8 py-5">
       <div className="flex flex-col md:flex-row md:items-center justify-between">
         <div className="flex items-center">
           <Link
--- my-portfolio/app/contact/page.tsx
+++ my-portfolio/app/contact/page.tsx
@@ -1,9 +1,17 @@
+import GridContacts from "./grid-contacts";
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-4xl font-bold">Contact Page</h1>
-      </main>
-    </div>
+    <>
+      <div className="flex flex-col items-start py-2">
+        <div className="transform">
+          <h1 className="text-3xl font-bold mb-2">Contact Page</h1>
+          <p className="text-sm mb-4">Feel free to reach out!</p>
+        </div>
+      </div>
+      <div className="flex flex-col items-start py-2">
+        <p className="text-sm mb-4">Connect through here.</p>
+        <GridContacts />
+      </div>
+    </>
   );
 }
